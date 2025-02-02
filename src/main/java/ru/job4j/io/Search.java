df9fe7b5b61f9fd0678ff4fc8c6d1d5f148d1092
Markesoon <saiferdk@mail.ru>
2025-02-02 18:37:06 +0300

    13. Именованные аргументы [#295518]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,22 +1,37 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
-        }
+        validation(args);
         Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validation(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Arguments length not equal 2");
+        }
+        Path start = Paths.get(args[0]);
+        if (!Files.isDirectory(start) || !Files.exists(start)) {
+            throw new IllegalArgumentException(String.format("Root folder does not exist"));
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException(String.format("Not a file"));
+        }
     }
 }
