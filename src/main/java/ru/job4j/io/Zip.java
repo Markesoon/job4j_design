032d9644b3f09132eab1040abf48034fffcdc10e
Markesoon <saiferdk@mail.ru>
2025-02-03 21:35:12 +0300

    14. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -58,7 +58,7 @@ public class Zip {
         ArgsName argsName = ArgsName.of(args);
         if (validation(argsName)) {
             Path start = Paths.get(argsName.get("d"));
-            zip.packFiles(Search.search(start, p -> p.toFile().getName().
+            zip.packFiles(Search.search(start, p -> !p.toFile().getName().
                             endsWith(argsName.get("e"))),
                     new File(argsName.get("o")));
         }
