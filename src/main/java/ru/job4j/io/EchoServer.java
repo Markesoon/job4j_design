b32b317b02d8172ee7445d4fdb43f96a83e6066f
Markesoon <saiferdk@mail.ru>
2025-03-17 20:37:55 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,10 +1,16 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -15,18 +21,21 @@ public class EchoServer {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     output.write(("Hello\r\n\r\n".getBytes()));
                     String string = input.readLine();
-                        if (string.contains("msg=What")) {
-                            output.write("What\r\n\r\n".getBytes());
-                            System.out.println(string);
-                        }
+                    if (string.contains("msg=What")) {
+                        output.write("What\r\n\r\n".getBytes());
+                        System.out.println(string);
+                    }
                     if (string.contains("msg=Exit")) {
                         output.write("Завершить работу сервера\r\n\r\n".getBytes());
                         server.close();
                     }
                     output.flush();
+                } catch (IOException e) {
+                    LOG.error("Exception while starting server", e);
                 }
             }
+        } catch (IOException e) {
+            LOG.error("Exception while handling client connection", e);
         }
     }
-
-}
+    }
